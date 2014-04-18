d0f2087c5ab91699582ae5c710418369013364a1
Jim Ruppert <ruppert@tetrahedra.com>
2014-04-18 00:25:31 -0700

    solution

---
+++ Problem7.java
@@ -0,0 +1,8 @@
+// www.projecteuler.net
+public class Problem7 {
+
+	public static void main(String [] args) {
+		Primes primeGetter = new Primes();
+		System.out.println(primeGetter.getNth(10001));
+	}
+}
